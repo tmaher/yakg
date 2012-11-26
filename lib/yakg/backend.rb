8aa6d5e9ce67f99832620c1cfe6732cafeaccf01
Tom Maher <tmaher@heroku.com>
2012-11-25 20:05:58 -0800

    wip

--- lib/yakg/backend.rb
+++ lib/yakg/backend.rb
@@ -14,14 +14,20 @@ class Yakg
     #typealias "UInt32", "unsigned int"
     
     #extern "OSStatus SecKeychainFindGenericPassword(CFTypeRef, UInt32, const char *, UInt32, const char *, UInt32 *, void **, void *)"
+
+    def self.framework name
+      "/System/Library/Frameworks/#{name}.framework/#{name}"
+    end
+
     extend FFI::Library
-    framework_dir = "/System/Library/Frameworks"
-    ffi_lib(FFI::Library::LIBC,
-            "#{framework_dir}/Security.framework/Versions/A/Security",
-            "/usr/lib/libm.dylib")
     NULL = FFI::Pointer::NULL
+    ffi_lib(FFI::Library::LIBC,
+            framework(:CoreFoundation),
+            framework(:Security))
+
     attach_function :malloc, [:size_t], :pointer
     attach_function :free, [:pointer], :void
+    attach_function :CFRelease, [:pointer], :void
 
     attach_function(:SecKeychainItemFreeContent,
                     [:pointer, :pointer], :uint32)
@@ -37,6 +43,29 @@ class Yakg
                     :uint32)
 
     attach_function(:SecKeychainItemDelete, [:pointer], :uint32)
+    attach_function(:SecCopyErrorMessageString, [:uint32, :pointer],
+                    :pointer)
+
+    attach_function(:CFStringGetLength, [:pointer], :int64)
+    attach_function(:CFStringGetCString,
+                    [:pointer, :pointer, :long, :uint32], :bool)
+
+    def self.error_message code
+      msg_cf_ref = SecCopyErrorMessageString(code, NULL)
+      #return msg_cf_ref.methods
+      #CFStringGetLength(msg_cf_ref)
+      #return msg_cf_ref.read_pointer.to_i
+
+      # utf-8 chars are guaranteed to be <= 4 bytes
+      # 0x08000100 is the apple magic number for UTF-8 encoding
+      c_buffer = malloc(400)
+      CFStringGetCString(msg_cf_ref.read_pointer,
+                         c_buffer.read_pointer,
+                         400, 0x08000100)
+      retstring = c_buffer.read_string
+      free c_buffer.read_pointer
+      retstring
+    end
     
     def self.delete acct, svc
       pw_length = FFI::MemoryPointer.new :uint32
@@ -48,7 +77,8 @@ class Yakg
                                               item_ref)
       SecKeychainItemFreeContent(NULL, pw_val.read_pointer)
       SecKeychainItemDelete(item_ref.read_pointer)
-      
+      puts error_message(-25294)
+      CFRelease item_ref.read_pointer
     end
 
     def self.get acct, svc
