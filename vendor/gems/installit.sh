cba5ee4163498238c96ee287b00bbf7c08286056
Tom Maher <tmaher@heroku.com>
2013-08-02 18:52:58 -0700

    total rewrite for the new approach of using non-universal ruby, but universal .bundle's

--- vendor/gems/installit.sh
+++ vendor/gems/installit.sh
@@ -1,44 +1,111 @@
 #!/bin/bash -x
 
-if [ "x$1" = "appleruby" ]; then
-    export PATH="/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:/usr/local/sbin"
-fi
-DIR="$( cd "$( dirname "$0" )" && pwd )" ; cd $DIR
+set -e
 
-if ! [ -r `basename $0 2>/dev/null` ]; then
-    echo "FAIL! can't cd to the parent of $0"
-    exit 1
-fi
+export R200="2.0.0-p195"
+export R191="1.9.3-p448"
+export R18="1.8.7-p374"
 
-export RUBY_ABI_VERSION=`ruby --version | cut -d \  -f 2 | cut -d . -f 1-2`
+cd ${HOME}/heroku/yakg/vendor/gems/ruby
+git rm -r *
+rm -rf *
+mkdir -p x86_64/2.0.0 x86_64/1.9.1 x86_64/1.8 i386/2.0.0 i386/1.9.1 i386/1.8
 
-if [ "x${RUBY_ABI_VERSION}" = "x" ]; then
-    echo "FAIL!  can't figure out ruby version"
-    exit 2
-fi
+pushd ${HOME}/.rbenv/versions
+ln -sf x86_64-${R200} ${R200}
+ln -sf x86_64-${R191} ${R191}
+ln -sf x86_64-${R18} ${R18}
+popd
 
-if echo ${RUBY_ABI_VERSION} 2>/dev/null | grep "^1\.8" 2>/dev/null; then
-    export RUBY_ABI_VERSION=1.8
-elif echo ${RUBY_ABI_VERSION} 2>/dev/null | grep "^1\.9" 2>/dev/null; then
-    export RUBY_ABI_VERSION=1.9.1
-elif echo ${RUBY_ABI_VERSION} 2>/dev/null | grep "^2\.0" 2>/dev/null; then
-    export RUBY_ABI_VERSION=2.0.0
-fi
+pushd x86_64
 
-rm -rf Gemfile.lock "ruby/${RUBY_ABI_VERSION}"
-bundle install --path `pwd`
+echo ${R200} > ${HOME}/.rbenv/version
+gem install ffi corefoundation -i ./2.0.0
+pushd 2.0.0
+rm -rf cache doc
+pushd gems
+rm -rf */spec */ext */libtest
+popd
+popd
 
-FFI_C=`find ruby/${RUBY_ABI_VERSION} -type f -name ffi_c.bundle | head -n 1`
-if [ "x${FFI_C}" = "x" ]; then
-    echo "FAIL!  Couldn't install ffi"
-    exit 3
-fi
+echo ${R191} > ${HOME}/.rbenv/version
+gem install ffi corefoundation -i ./1.9.1
+pushd 1.9.1
+rm -rf cache doc
+pushd gems
+rm -rf */spec */ext */libtest
+popd
+popd
 
-CFRB=`find ruby/${RUBY_ABI_VERSION} -type f -name corefoundation.rb | head -n 1`
-if [ "x${CFRB}" = "x" ]; then
-    echo "FAIL!  Couldn't install corefoundation"
-    exit 4
-fi
+echo ${R18} > ${HOME}/.rbenv/version
+gem install ffi corefoundation -i ./1.8
+pushd 1.8
+rm -rf cache doc
+pushd gems
+rm -rf */spec */ext */libtest
+popd
+popd
 
-rm -rf ./ruby/${RUBY_ABI_VERSION}/gems/ffi-*/{ext,spec}
-rm -rf ./ruby/${RUBY_ABI_VERSION}/cache
+popd
+
+pushd ${HOME}/.rbenv/versions
+ln -sf i386-${R200} ${R200}
+ln -sf i386-${R191} ${R191}
+ln -sf i386-${R18} ${R18}
+echo ${R191} > ../version
+popd
+
+
+pushd i386
+
+
+echo ${R200} > ${HOME}/.rbenv/version
+gem install ffi corefoundation -i ./2.0.0
+pushd 2.0.0
+rm -rf cache doc
+pushd gems
+rm -rf */spec */ext */libtest
+popd
+popd
+
+echo ${R191} > ${HOME}/.rbenv/version
+gem install ffi corefoundation -i ./1.9.1
+pushd 1.9.1
+rm -rf cache doc
+pushd gems
+rm -rf */spec */ext */libtest
+popd
+popd
+
+echo ${R18} > ${HOME}/.rbenv/version
+gem install ffi corefoundation -i ./1.8
+pushd 1.8
+rm -rf cache doc
+pushd gems
+rm -rf */spec */ext */libtest
+popd
+popd
+
+popd
+
+mv x86_64/* .
+rmdir x86_64
+
+lipo i386/2.0.0/gems/ffi-*/lib/ffi_c.bundle 2.0.0/ffi-*/lib/ffi_c.bundle \
+    -create -output 2.0.0/ffi-*/lib/ffi_c_universal.bundle
+lipo i386/1.9.1/gems/ffi-*/lib/ffi_c.bundle 1.9.1/ffi-*/lib/ffi_c.bundle \
+    -create -output 1.9.1/ffi-*/lib/ffi_c_universal.bundle
+lipo i386/1.8/gems/ffi-*/lib/ffi_c.bundle 1.8/ffi-*/lib/ffi_c.bundle \
+    -create -output 1.8/ffi-*/lib/ffi_c_universal.bundle
+
+rm */ffi-*/lib/ffi_c.bundle
+
+pushd 2.0.0/ffi-*/lib
+mv ffi_c_universal.bundle ffi_c.bundle
+popd
+pushd 1.9.1/ffi-*/lib
+mv ffi_c_universal.bundle ffi_c.bundle
+popd
+pushd 1.8/ffi-*/lib
+mv ffi_c_universal.bundle ffi_c.bundle
+popd
