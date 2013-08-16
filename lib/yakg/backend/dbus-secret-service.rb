0c08db69177207bb4df51a5fb92fd27ffc57fe64
Tom Maher <tmaher@heroku.com>
2013-08-15 19:11:29 -0700

    rename to secret_service for consistency

--- lib/yakg/backend/dbus-secret-service.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'dbus'
-
-bus = DBus::SystemBus.instance
---
+++ lib/yakg/backend/secret_service.rb
@@ -0,0 +1,23 @@
+require "secretservice"
+
+class Yakg
+module Backend
+module SecretService
+  SS = SecretService.new
+
+  def set acct, value, svc
+  end
+
+  def get acct, svc
+    
+  end
+
+  def delete acct, svc
+  end
+
+  def list svc
+  end
+
+end
+end
+end
